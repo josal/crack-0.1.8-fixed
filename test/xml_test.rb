917594aa774bf07cbe93189eb21f0eabe6ce0f50
José Salinas <jose@rentages.com>
2013-01-11 19:35:08 +0100

    Removing yaml and symbol support to fix rails vulnerability

--- test/xml_test.rb
+++ test/xml_test.rb
@@ -216,7 +216,6 @@ class XmlTest < Test::Unit::TestCase
         <approved type="boolean"></approved>
         <written-on type="date"></written-on>
         <viewed-at type="datetime"></viewed-at>
-        <content type="yaml"></content>
         <parent-id></parent-id>
       </topic>
     EOT
@@ -227,7 +226,6 @@ class XmlTest < Test::Unit::TestCase
       'approved'   => nil,
       'written_on' => nil,
       'viewed_at'  => nil,
-      'content'    => nil,
       'parent_id'  => nil
     }
     Crack::XML.parse(topic_xml)["topic"].should == expected_topic_hash
@@ -244,7 +242,6 @@ class XmlTest < Test::Unit::TestCase
         <replies-close-in type="integer">2592000000</replies-close-in>
         <written-on type="date">2003-07-16</written-on>
         <viewed-at type="datetime">2003-07-16T09:28:00+0000</viewed-at>
-        <content type="yaml">--- \n1: should be an integer\n:message: Have a nice day\narray: \n- should-have-dashes: true\n  should_have_underscores: true\n</content>
         <author-email-address>david@loudthinking.com</author-email-address>
         <parent-id></parent-id>
         <ad-revenue type="decimal">1.5</ad-revenue>
@@ -262,15 +259,11 @@ class XmlTest < Test::Unit::TestCase
       'replies_close_in' => 2592000000,
       'written_on' => Date.new(2003, 7, 16),
       'viewed_at' => Time.utc(2003, 7, 16, 9, 28),
-      # Changed this line where the key is :message.  The yaml specifies this as a symbol, and who am I to change what you specify
-      # The line in ActiveSupport is
-      # 'content' => { 'message' => "Have a nice day", 1 => "should be an integer", "array" => [{ "should-have-dashes" => true, "should_have_underscores" => true }] },
-      'content' => { :message => "Have a nice day", 1 => "should be an integer", "array" => [{ "should-have-dashes" => true, "should_have_underscores" => true }] },
       'author_email_address' => "david@loudthinking.com",
       'parent_id' => nil,
       'ad_revenue' => BigDecimal("1.50"),
       'optimum_viewing_angle' => 135.0,
-      'resident' => :yes
+      'resident' => 'yes'
     }
 
     Crack::XML.parse(topic_xml)["topic"].each do |k,v|
